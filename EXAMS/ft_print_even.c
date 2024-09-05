d7c51bb614bb3043e544ffa04013cd138ae52190
humanur <127956689+humanur@users.noreply.github.com>
2024-09-05 15:50:48 +0300

    EXAMS

---
+++ EXAMS/ft_print_even.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void ft_print_even(char *str) 
+{
+    int i = 0;
+    while (str[i] != '\0') 
+	{
+		if (i % 2 == 0) 
+		{
+            printf("%c", str[i]);
+        }
+        i++;
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    int j = 1;
+    while (j < argc) 
+	{
+		ft_print_even(argv[j]);
+		if (j < argc - 1) 
+		{
+			printf(" ");
+		}
+        j++;
+    }
+	printf("\n");
+
+    return 0;
+}
