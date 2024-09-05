ce0ffb37ff4c0c0b45a5ad4bcb9d33af5a5fc136
humanur <127956689+humanur@users.noreply.github.com>
2024-09-05 14:47:05 +0300

    C00

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amsagman <amsagman@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 15:14:00 by amsagman          #+#    #+#             */
+/*   Updated: 2024/08/19 16:52:29 by amsagman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	char	negative;
+	char	positive;
+
+	negative = 'N';
+	positive = 'P';
+	if (n >= 0)
+	{
+		write(1, &positive, 1);
+	}
+	else
+	{
+		write(1, &negative, 1);
+	}
+}
---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amsagman <amsagman@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 14:40:54 by amsagman          #+#    #+#             */
+/*   Updated: 2024/08/19 16:59:03 by amsagman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(int a, int b)
+{
+	ft_putchar((a / 10) + '0');
+	ft_putchar((a % 10) + '0');
+	ft_putchar(' ');
+	ft_putchar((b / 10) + '0');
+	ft_putchar((b % 10) + '0');
+	if (a < 98)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x <= 98)
+	{
+		y = x + 1;
+		while (y <= 99)
+		{
+			ft_print_numbers(x, y);
+			y++;
+		}
+		x++;
+	}
+}
