9d53cf955d1679ec0084d22b7425ff0751b64c20
humanur <127956689+humanur@users.noreply.github.com>
2024-09-05 14:47:22 +0300

    C01

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amsagman <amsagman@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 20:44:21 by amsagman          #+#    #+#             */
+/*   Updated: 2024/08/20 17:18:09 by amsagman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				swap = tab [i];
+				tab[i] = tab [i + 1];
+				tab [i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
