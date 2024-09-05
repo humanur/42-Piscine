ffa116f9dafebf0ce804dcf5bec21c9f5c0f9eb6
humanur <127956689+humanur@users.noreply.github.com>
2024-09-05 19:42:45 +0300

    Add files via upload

---
+++ EXAMS/atoi.c
@@ -0,0 +1,43 @@
+
+int	atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+	int cont;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	cont = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+			cont++;
+		}
+		i++;
+	}
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	if (cont > 1)
+		return 0;
+	return (result * sign);
+}
+
+#include <stdio.h>
+int main(int ac, char **av)
+{
+	if (ac == 2)
+		printf("%d\n", atoi(av[1]));
+
+	/*char	*str = "   ---+--+01234ab567"; //argüman almadan çıktı alabilmek için maindeki ac ve **av silinmeli.
+	printf("%d", atoi(str));*/ 
+}
